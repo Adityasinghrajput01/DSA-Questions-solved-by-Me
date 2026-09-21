6bf78c54a48d8aaf619c8bbd7e632ff2307995d5
aditya kumar singh <adityasingh45245@gmail.com>
2026-09-22 00:06:18 +0530

    Time: 89 ms (70.18%), Space: 247.3 MB (60.39%) - LeetHub

---
+++ 4049-count-values-with-equally-spaced-occurrences-ii/4049-count-values-with-equally-spaced-occurrences-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countSpecialIntegers(int[] nums) {
+        HashMap<Integer,List<Integer>>map = new HashMap<>();
+        for(int i  = 0;i<nums.length;i++){
+           map.computeIfAbsent(nums[i],k->new ArrayList<>()).add(i); 
+        }
+        int count = 0;
+        for (List<Integer>pos : map.values()) {
+            if (pos.size() < 3)
+          continue;
+         int gap = pos.get(1) - pos.get(0);
+            boolean s = true;
+            for (int j = 2; j < pos.size(); j++) {
+                int currentgap = pos.get(j) - pos.get(j - 1);
+                if (currentgap != gap) {
+                    s = false;
+                    break;
+                }
+            }
+            if (s==true) {
+                count++;
+            }
+}
+   return count;
+    }
+} 
